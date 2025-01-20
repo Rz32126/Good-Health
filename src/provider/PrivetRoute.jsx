1908f8b8e592283e65e63a0318ed9b83bdfc20a0
Rz32126 <rifatzahan11@gmail.com>
2025-01-20 11:19:11 +0600

    add all thet files

--- src/provider/PrivetRoute.jsx
+++ src/provider/PrivetRoute.jsx
@@ -9,7 +9,7 @@ const PrivetRoute = ({children}) => {
 
     const { user, loading } = useContext(AuthContext);
     const location = useLocation();
-    console.log(location)
+    // console.log(location)
     if(loading){
       return <LoadingSpinner></LoadingSpinner>
     }
