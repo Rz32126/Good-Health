588f8a4b01d6b6d387f86dfd96a86266b51298d5
Rz32126 <rifatzahan11@gmail.com>
2025-02-19 14:26:03 +0600

    add the update things

---
+++ src/components/Past.jsx
@@ -0,0 +1,30 @@
+import { FaHandshake } from "react-icons/fa";
+
+const Past = () => {
+    return (
+        <div>
+            <div>
+            <p>We are served since 35 years to helpless people with trust and we are happy<br></br>
+            be Part of us
+            </p>
+            </div>
+            <div>
+              <span><FaHandshake /></span>
+              <p>365</p>
+              <p>Total Awards</p>
+            </div>
+            <div>
+              <span><FaHandshake /></span>
+              <p>365</p>
+              <p>Total Awards</p>
+            </div>
+            <div>
+              <span><FaHandshake /></span>
+              <p>365</p>
+              <p>Total Awards</p>
+            </div>
+        </div>
+    );
+};
+
+export default Past;
